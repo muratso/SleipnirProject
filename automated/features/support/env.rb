0b62ca80339d602b4188517027254aa7f4a788be
muratso <rockstar2304@gmail.com>
2017-01-29 19:46:19 -0200

    [FIX] Changing web browser drive

--- automated/features/support/env.rb
+++ automated/features/support/env.rb
@@ -4,18 +4,18 @@ require "rspec"
 require 'capybara/poltergeist'
 require 'selenium-webdriver'
 
-Capybara.default_driver = :chrome
-Capybara.register_driver :chrome do |app|
-  options = {
-      :js_errors => false,
-      :timeout => 360,
-      :debug => false,
-      :inspector => false,
-  }
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
-
-Capybara.javascript_driver = :chrome
+# Capybara.default_driver = :chrome
+# Capybara.register_driver :chrome do |app|
+#   options = {
+#       :js_errors => false,
+#       :timeout => 360,
+#       :debug => false,
+#       :inspector => false,
+#   }
+#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
+# end
+#
+# Capybara.javascript_driver = :chrome
 
 # before_scenario do
 #   $PASSWORD = ENV['PASSWORD']
@@ -26,14 +26,14 @@ $PASSWORD = ENV['PASSWORD']
 $USERNAME = ENV['USERNAME']
 
 
-# Capybara.default_driver = :poltergeist
-# Capybara.register_driver :poltergeist do |app|
-#   options = {
-#       :js_errors => true,
-#       :timeout => 120,
-#       :debug => false,
-#       :phantomjs_options => ['--load-images=no', '--disk-cache=false'],
-#       :inspector => true,
-#   }
-#   Capybara::Poltergeist::Driver.new(app, options)
-# end
+Capybara.default_driver = :poltergeist
+Capybara.register_driver :poltergeist do |app|
+  options = {
+      :js_errors => true,
+      :timeout => 120,
+      :debug => false,
+      :phantomjs_options => ['--load-images=no', '--disk-cache=false'],
+      :inspector => true,
+  }
+  Capybara::Poltergeist::Driver.new(app, options)
+end
